7640a8f48e4f5a6dec204a4ffc109f1e76f5c61b
Joan Massich <sik@visor.udg.edu>
2016-08-01 13:06:13 +0200

    move code from main to library

--- src/common/szl.h
+++ src/common/szl.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <iostream>
+
+class Sink {
+  std::string file_name = "/tmp/log.txt" ;
+ public:
+  void log(){std::cout << "hi Sink" << std::endl;} ;
+};
+
+class Logger {
+  enum LOGGIG_LEVELS {CRITIC, ERROR, WARNING, INFO, CRAZY} ;
+  LOGGIG_LEVELS my_logging_level ;
+ public:
+ Logger( const LOGGIG_LEVELS _level=CRITIC ) : my_logging_level(_level) {} ;
+  void log(){std::cout << "hi Logger says " << my_logging_level << std::endl;} ;
+} ;
