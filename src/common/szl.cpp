10cde2c2639f3ec52d4376815e5ea4e5019ff8aa
Joan Massich <sik@visor.udg.edu>
2016-08-03 12:06:19 +0200

    Added << operator but it does not work

--- src/common/szl.cpp
+++ src/common/szl.cpp
@@ -9,3 +9,36 @@ void Sink::log (std::string message)
 {
   file << message ;
 }
+
+std::ostream& operator<< (std::ostream& os, LogLevel level)
+{
+  switch(level)
+    {
+    case LogLevel::FATAL   : os << "fatal";   break;
+    case LogLevel::CRITIC  : os << "critic";  break;
+    case LogLevel::ERROR   : os << "error";   break;
+    case LogLevel::WARNING : os << "warning"; break;
+    case LogLevel::INFO    : os << "info";    break;
+    case LogLevel::TRACE   : os << "trace";   break;
+    case LogLevel::CRAZY   : os << "crazy";   break;
+    default    : os.setstate(std::ios_base::failbit);
+    }
+  return os;
+}
+
+std::ostream& operator<< (std::ostream& os, LogLevel level)
+{
+  switch(level)
+    {
+    case LogLevel::FATAL   : os << "fatal";   break;
+    case LogLevel::CRITIC  : os << "critic";  break;
+    case LogLevel::ERROR   : os << "error";   break;
+    case LogLevel::WARNING : os << "warning"; break;
+    case LogLevel::INFO    : os << "info";    break;
+    case LogLevel::TRACE   : os << "trace";   break;
+    case LogLevel::CRAZY   : os << "crazy";   break;
+    default    : os.setstate(std::ios_base::failbit);
+    }
+  return os;
+}
+
--- src/common/szl.h
+++ src/common/szl.h
@@ -5,19 +5,32 @@
 #include <fstream>
 
 class Sink {
+ protected:
   const std::string file_name ;
   std::ofstream file ;
+
  public:
   Sink (const std::string _file_name="/tmp/log.txt") : file_name(_file_name) {file.open(file_name);};
   ~Sink() ;
   void log (const std::string message) ;
+
 };
 
+/* class NullSink : public Sink { */
+/*  public: */
+/*   void log (const std::string message) {}; */
+/* }; */
+
+
+enum class LogLevel {FATAL, CRITIC, ERROR, WARNING, INFO, TRACE, CRAZY} ;
+
 class Logger {
-  enum LOGGIG_LEVELS {CRITIC, ERROR, WARNING, INFO, CRAZY} ;
-  LOGGIG_LEVELS my_logging_level ;
-  Sink s ;
+ private:
+  LogLevel my_logging_level ;
+  Sink* s ;
+
  public:
- Logger( const LOGGIG_LEVELS _level=CRITIC ) : my_logging_level(_level) {} ;
+ Logger( const LogLevel _level=LogLevel::CRITIC, Sink* s=nullptr) : my_logging_level(_level) {} ;
   void log(){std::cout << "hi Logger says " << my_logging_level << std::endl;} ;
 } ;
+
