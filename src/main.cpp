be8e3b00d3ef82f69d1eab7814ad33ae84d2a44e
Joan Massich <sik@visor.udg.edu>
2016-08-06 16:37:25 +0200

    perfect forwarding stl error..

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 #include <iostream>
 #include "common/szl.h"
 
+decltype(auto) xx(LogLevel l, std::ostream& os){
+  return os << "[" << "xx" << "]: ";
+}
+
+decltype(auto) give_me_the_fucking_cout()
+{
+  return std::cout << "hi";
+}
+
 int main()
 {
   LOG() << "hello world, the default logging is FATAL\n";
   LOG(LogLevel::INFO) << "this is an INFO message\n";
+
+  xx(LogLevel::INFO, give_me_the_fucking_cout()) << "this is xx function called from main\n";
 }
