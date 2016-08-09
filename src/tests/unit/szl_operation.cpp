67e6db0583ed5f62bad4f18e248a3506322c2a40
Joan Massich <sik@visor.udg.edu>
2016-08-09 20:55:27 +0200

    Added test Fixtures

---
+++ src/tests/unit/szl_operation.cpp
@@ -0,0 +1,85 @@
+#include "tests/unit/szl_operation.h"
+// TEST(foo,critic){}
+// TEST(foo,error){}
+// TEST(foo,warning){}
+// TEST(foo,info){}
+// TEST(foo,trace){}
+// TEST(foo,crazy){}
+
+TEST(foo, log_levels){
+  std::ostringstream local;
+  auto cout_buff = std::cout.rdbuf();
+  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+  LOG(LogLevel::FATAL) << "test";
+  ASSERT_STREQ( "[fatal]: test", local.str().c_str() );
+  // go back to old buffer
+  std::cout.rdbuf(cout_buff);
+}
+
+TEST(foo, log_levels_default){
+  std::ostringstream local;
+  auto cout_buff = std::cout.rdbuf();
+  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+  LOG() << "test";
+  ASSERT_STREQ( "[fatal]: test", local.str().c_str() );
+  // go back to old buffer
+  std::cout.rdbuf(cout_buff);
+}
+
+TEST(foo, wrong_enum){
+  std::ostringstream local;
+  auto cout_buff = std::cout.rdbuf();
+  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+  try {
+    LOG( static_cast<LogLevel>(-1) ) << "test";
+  } catch(std::bad_cast const & err) {
+    EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
+  }
+  // go back to old buffer
+  std::cout.rdbuf(cout_buff);
+}
+
+TEST(foo, wrong_enum2){
+  std::ostringstream local;
+  try {
+    local << static_cast<LogLevel>(-1);
+  } catch(std::bad_cast const & err) {
+    EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
+  }
+}
+
+TEST(foo, log_unknown_object){
+  /* Here you don't see if the problem is with streaming the LogLevel or the
+   * object after it
+   */
+
+  std::ostringstream local;
+  auto cout_buff = std::cout.rdbuf();
+  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+  try {
+    LOG() << NULL;
+  } catch(std::bad_cast const & err) {
+    EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
+  }
+  // go back to old buffer
+  std::cout.rdbuf(cout_buff);
+}
+
+struct UnknownObject{};
+TEST(foo, log_unknown_object2){
+  /* Here you don't see if the problem is with streaming the LogLevel or the
+   * object after it
+   */
+
+  std::ostringstream local;
+  UnknownObject unknwn_obj;
+  auto cout_buff = std::cout.rdbuf();
+  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+  try {
+    LOG() << static_cast<void*>(&unknwn_obj) ; //void* is the most basic type available for <<
+  } catch(std::bad_cast const & err) {
+    EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
+  }
+  // go back to old buffer
+  std::cout.rdbuf(cout_buff);
+}
--- src/tests/unit/szl_operation.h
+++ src/tests/unit/szl_operation.h
@@ -1,49 +1,66 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <strstream>
 #include "common/szl.h"
 
-// TEST(foo,critic){}
-// TEST(foo,error){}
-// TEST(foo,warning){}
-// TEST(foo,info){}
-// TEST(foo,trace){}
-// TEST(foo,crazy){}
 
-TEST(foo, log_levels){
-  std::ostringstream local;
-  auto cout_buff = std::cout.rdbuf();
-  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+class szlBasicFixture: public ::testing::Test {
+ public:
+  szlBasicFixture( ) {
+    // initialization code here
+  }
+
+  void SetUp( ) {
+    // code here will execute just before the test ensues
+    captured_stream = new std::ostringstream();
+    cout_buff = std::cout.rdbuf();
+    std::cout.rdbuf(captured_stream->rdbuf()); // cout-buffer points to local
+
+  }
+
+  void TearDown( ) {
+    // code here will be called just after the test completes
+    // ok to through exceptions from here if need be
+    delete(captured_stream);
+    std::cout.rdbuf(cout_buff);
+  }
+
+  ~szlBasicFixture( )  {
+    // cleanup any pending stuff, but no exceptions allowed
+  }
+
+  // put in any custom data members that you need
+  std::ostringstream * captured_stream;
+  std::strstreambuf::basic_streambuf * cout_buff;
+};
+
+// TEST(szlBasicFixture,critic){}
+// TEST(szlBasicFixture,error){}
+// TEST(szlBasicFixture,warning){}
+// TEST(szlBasicFixture,info){}
+// TEST(szlBasicFixture,trace){}
+// TEST(szlBasicFixture,crazy){}
+
+TEST_F(szlBasicFixture, log_levels){
   LOG(LogLevel::FATAL) << "test";
-  ASSERT_STREQ( "[fatal]: test", local.str().c_str() );
-  // go back to old buffer
-  std::cout.rdbuf(cout_buff);
+  ASSERT_STREQ( "[fatal]: test", captured_stream->str().c_str() );
 }
 
-TEST(foo, log_levels_default){
-  std::ostringstream local;
-  auto cout_buff = std::cout.rdbuf();
-  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+TEST_F(szlBasicFixture, log_levels_default){
   LOG() << "test";
-  ASSERT_STREQ( "[fatal]: test", local.str().c_str() );
-  // go back to old buffer
-  std::cout.rdbuf(cout_buff);
+  ASSERT_STREQ( "[fatal]: test", captured_stream->str().c_str() );
 }
 
-TEST(foo, wrong_enum){
-  std::ostringstream local;
-  auto cout_buff = std::cout.rdbuf();
-  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
+TEST_F(szlBasicFixture, wrong_enum){
   try {
     LOG( static_cast<LogLevel>(-1) ) << "test";
   } catch(std::bad_cast const & err) {
     EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
   }
-  // go back to old buffer
-  std::cout.rdbuf(cout_buff);
 }
 
-TEST(foo, wrong_enum2){
+TEST_F(szlBasicFixture, wrong_enum2){
   std::ostringstream local;
   try {
     local << static_cast<LogLevel>(-1);
@@ -52,38 +69,28 @@ TEST(foo, wrong_enum2){
   }
 }
 
-TEST(foo, log_unknown_object){
+TEST_F(szlBasicFixture, log_unknown_object){
   /* Here you don't see if the problem is with streaming the LogLevel or the
    * object after it
    */
 
-  std::ostringstream local;
-  auto cout_buff = std::cout.rdbuf();
-  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
   try {
     LOG() << NULL;
   } catch(std::bad_cast const & err) {
     EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
   }
-  // go back to old buffer
-  std::cout.rdbuf(cout_buff);
 }
 
 struct UnknownObject{};
-TEST(foo, log_unknown_object2){
+TEST_F(szlBasicFixture, log_unknown_object2){
   /* Here you don't see if the problem is with streaming the LogLevel or the
    * object after it
    */
 
-  std::ostringstream local;
   UnknownObject unknwn_obj;
-  auto cout_buff = std::cout.rdbuf();
-  std::cout.rdbuf(local.rdbuf()); // cout-buffer points to local
   try {
     LOG() << static_cast<void*>(&unknwn_obj) ; //void* is the most basic type available for <<
   } catch(std::bad_cast const & err) {
     EXPECT_EQ( std::string("Wrong LogLevel"), err.what() );
   }
-  // go back to old buffer
-  std::cout.rdbuf(cout_buff);
 }
