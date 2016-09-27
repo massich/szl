b579e4497cf9817c087ab33ccc7a2e8074a86181
Joan Massich <sik@visor.udg.edu>
2016-09-27 19:15:26 +0200

    added to download gtest through git

--- src/cmake/find_or_download_gtest.cmake
+++ src/cmake/find_or_download_gtest.cmake
@@ -1,11 +1,47 @@
-## Find Google Test
 find_package(GTest)
 
 if (NOT GTest_FOUND)
-  message("GTest not found")
+  ExternalProject_Add(
+      googletest-master
+      DOWNLOAD_DIR "googletest-master/src" # The only dir option which is required
+      GIT_REPOSITORY https://github.com/google/googletest.git
+      TIMEOUT 10
+      LOG_DOWNLOAD ON
+      # Disable all other steps
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ""
+  )
+
+  # # Build gtest from existing sources
+  # ExternalProject_Add(
+  #     gtest
+  #     DOWNLOAD_COMMAND "" # No download required
+  #     SOURCE_DIR "googletest-master/src/googletest" # Use specific source dir
+  #     PREFIX "googletest-master" # But use prefix for compute other dirs
+  #     INSTALL_COMMAND ""
+  #     LOG_CONFIGURE ON
+  #     LOG_BUILD ON
+  # )
+
+  # # gtest should be build after being downloaded
+  # add_dependencies(gtest googletest-master)
+
+  # # Build gmock from existing sources
+  # ExternalProject_Add(
+  #     gmock
+  #     DOWNLOAD_COMMAND "" # No download required
+  #     SOURCE_DIR "googletest-master/src/googlemock" # Use specific source dir
+  #     PREFIX "googletest-master" # But use prefix for compute other dirs
+  #     INSTALL_COMMAND ""
+  #     LOG_CONFIGURE ON
+  #     LOG_BUILD ON
+  # )
+
+  # # gmock should be build after being downloaded
+  # add_dependencies(gmock googletest-master)
 
 else(NOT GTest_FOUND)
-  message("Gtest found: ${GTEST_INCLUDE_DIRS}")
   include_directories(${GTEST_INCLUDE_DIRS})
 
 endif(NOT GTest_FOUND)
